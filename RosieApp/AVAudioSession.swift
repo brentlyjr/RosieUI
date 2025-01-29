425811c3d72228de01290b2271e8272b9695953e
Brent Cromley <brentlyjr@yahoo.com>
2025-01-29 10:51:50 -0800

    Added changes to get it to work with the microphone on the phone.

--- RosieApp/AVAudioSession.swift
+++ RosieApp/AVAudioSession.swift
@@ -20,11 +20,15 @@ class AudioSessionManager {
         let audioSession = AVAudioSession.sharedInstance()
         do {
             // Set the audio category to allow simultaneous playback and recording
-            try audioSession.setCategory(.playAndRecord, options: [.defaultToSpeaker, .allowBluetooth])
+            try audioSession.setCategory(.playAndRecord, options: [.defaultToSpeaker, .allowBluetooth, .mixWithOthers])
             
             // Use the voiceChat mode to enable AEC and other voice optimizations
             try audioSession.setMode(.voiceChat)
             
+            // Manual set the audio route to the speaker, I added this when it wouldn't
+            // produce audio on the phone
+            try AVAudioSession.sharedInstance().overrideOutputAudioPort(.speaker)
+
             // Activate the audio session
             try audioSession.setActive(true)
             
